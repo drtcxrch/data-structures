494c7b89feb785b4184681ab8e1bb79a7368dbe9
Grant Taggart <granttaggartsf@gmail.com>
2020-10-22 11:21:18 -0700

    Cleaned master branch

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,25 +5,17 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
-  var count = 0;
 
   someInstance.push = function(value) {
-    storage[count] = value;
-    count += 1;
+
   };
 
   someInstance.pop = function() {
-    if (count >= 1) {
-      count -= 1;
-      var popped = storage[count];
-      delete storage[count];
-      return popped;
-    }
 
   };
 
   someInstance.size = function() {
-    return count;
+
   };
 
   return someInstance;
